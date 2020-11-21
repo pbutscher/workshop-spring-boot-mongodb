e2c0fc13ae356ccdf86a62d37f62964503aeb69c
Paulo Butscher <pbutscher@hotmail.es>
2020-11-21 19:01:03 -0300

    Obtendo um usuario por id

--- src/main/java/br/com/butscher/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/butscher/workshopmongo/resources/UserResource.java
@@ -1,12 +1,15 @@
 package br.com.butscher.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.butscher.workshopmongo.domain.User;
@@ -22,12 +25,14 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
-		// User maria = new User("1", "Maria", "maria@maria.com.br");
-		// User alex = new User("2", "Alex", "alex@alex.com.br");		
-		// List<User> list = new ArrayList<>();
-		// list.addAll(Arrays.asList(maria,alex));
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Optional<User>> findById(@PathVariable String id) {
+		Optional<User> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/br/com/butscher/workshopmongo/services/UserService.java
+++ src/main/java/br/com/butscher/workshopmongo/services/UserService.java
@@ -1,12 +1,16 @@
 package br.com.butscher.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.butscher.workshopmongo.domain.User;
 import br.com.butscher.workshopmongo.repository.UserRepository;
+import br.com.butscher.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +20,13 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public Optional<User> findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
